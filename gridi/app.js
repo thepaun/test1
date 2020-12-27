111d0240371cbfae3834ca818c099905a49023b2
Milan <paunmilan123@hotmail.rs>
2020-12-27 19:41:49 +0100

    test

---
+++ gridi/app.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('hamburger');
+
+hamburger.addEventListener('click', () => {
+    navi.classList.toggle('show');
+
+});
